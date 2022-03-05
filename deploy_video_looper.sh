8039fd588541c7b2cfc21fa9ef5cd49bd018eb36
theleiko <leiko@leiko.eu>
2022-03-05 16:17:32 +0100

    .

--- deploy_video_looper.sh
+++ deploy_video_looper.sh
@@ -18,29 +18,43 @@ apt update && apt full-upgrade -y
 # Some Basic Raspi config to be sure
 #raspi-config nonint do_hostname $HOSTNAME
 raspi-config nonint do_boot_splash 0
-raspi-config nonint do_overscan 0
+raspi-config nonint do_overscan 1
 raspi-config nonint do_pixdub 0
 raspi-config nonint do_memory_split 256
 raspi-config nonint do_boot_wait 0
 raspi-config nonint do_change_timezone Europe/Berlin 
-raspi-config nonint do_resolution %d %d
+# raspi-config nonint do_resolution %d %d
 
 # Expand Filesystem
 raspi-config nonint do_expand_rootfs
 
+echo -e "\033[31m--raspi-config done, installing mc\033[0m"
+sleep 2
+
 # Install mc as it should be in every install
-apt install mc -y
+apt install mc git -y
+
+
+echo -e "\033[31m--mc install done, installing pi-power-button\033[0m"
+sleep 2
 
 # Install Power Button Script
 cd /home/pi
+rm -rf /home/pi/pi_video_looper_config
+rm -rf /home/pi/pi_video_looper
+rm -rf /home/pi/pi-power-button
 git clone https://github.com/Howchoo/pi-power-button.git
 /home/pi/pi-power-button/script/install
 
+echo -e "\033[31m--pi-power-button install done, installing pi_video_looper\033[0m"
+sleep 2
 
 # Install pi_video_looper
 git clone https://github.com/adafruit/pi_video_looper
 /home/pi/pi_video_looper/install.sh no_hello_video
 
+echo -e "\033[31m--pi_video_looper install done, loading config\033[0m"
+sleep 2
 
 # Deploy the config for videolooper
 git clone https://github.com/theleiko/pi_video_looper_config
